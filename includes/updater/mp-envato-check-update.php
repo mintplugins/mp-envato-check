0f0436150f0310ce4ef32c9417f6afebabfe9ba3
Phil Johnston <johnstonphilip@gmail.com>
2013-05-17 14:45:16 -0400

    beta1.0.0.7
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/updater/mp-envato-check-update.php
+++ includes/updater/mp-envato-check-update.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Check for updates for this Theme
+ * Check for updates for this Plugin
  *
  */
  if (!function_exists('mp_envato_check_update')){
@@ -9,10 +9,10 @@
 			'software_name' => 'MP Envato Check', //<- The exact name of this Plugin. Make sure it matches the title in your mp_repo, edd, and the WP.org repo
 			'software_api_url' => 'http://moveplugins.com',//The URL where EDD and mp_repo are installed and checked
 			'software_filename' => 'mp-envato-check.php',
-			'software_licenced' => false, //<-Boolean
+			'software_licensed' => false, //<-Boolean
 		);
 		
-		//Since this is a theme, call the Plugin Updater class
+		//Since this is a plugin, call the Plugin Updater class
 		$mp_envato_check_plugin_updater = new MP_CORE_Plugin_Updater($args);
 	}
  }
